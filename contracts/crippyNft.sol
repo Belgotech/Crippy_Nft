99e212a1fab936992917183df289e0fceeaee890
belgotech <belovedobiora2002@gmail.com>
2022-01-01 06:10:08 +0100

    New

--- contracts/crippyNft.sol
+++ contracts/crippyNft.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import '@openzepplin/contracts/token/ERC721/ERC721.sol';
 import '@openzepplin/contracts/access/ownable .sol';
 
-contract crippyNft is ERC721, ownable {
+contract CrippyNft is ERC721, ownable {
     unit256 public mintPrice;
     unit256 public totalSupply;
     unit256 public maxSupply;
@@ -13,4 +13,11 @@ contract crippyNft is ERC721, ownable {
     string internal basetokenUri;
     address  payable public withdrawWallet;
     mapping(address => unit256) public mintWallet;
+
+    constructor() payable  ERC721('Crippy', 'CR') {
+    mintPrice = 0.02 ether;
+    totalSupply = 0;
+    maxSupply = 1000;
+    maxPerWallet = 3;
+    }  
 }
